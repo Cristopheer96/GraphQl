4fe8f3f335e97e8aef921745320c94d796a17aec
Cristopher Huaroc <cristopheer96@gmail.com>
2023-08-30 01:26:56 -0500

    make first query

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,7 +5,14 @@ module Types
     field :id, ID, null: false
     field :email, String
     field :name, String
+    field :posts, [Types::PostType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :post_count, Integer, null: true
   end
+  
+  def post_count
+    object.posts.count
+  end
+  
 end
